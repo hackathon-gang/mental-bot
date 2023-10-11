084943cf88e1fc9da9028bc3a6c04207cd6ff5b5
thinzarhninyu <thinzarhninyu.17@gmail.com>
2023-10-11 13:41:25 +0800

    saveChat, summarizeChat, saveSummary, giveSuggestions backend

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -4,6 +4,9 @@ import express from 'express';
 
 const router = express.Router();
 
-router.post('/api/user/chat', chatController.processChat);
+router.post('/api/user/chat/save', chatController.processSaveChat);
+router.post('/api/user/summary/save', chatController.processSaveSummary);
+router.post('/api/user/summary', chatController.processSummary);
+router.post('/api/user/suggestions', chatController.processSuggestion);
 
 export default router;
--- backend/src/services/chatServices.js
+++ backend/src/services/chatServices.js
@@ -1,18 +1,71 @@
-import config from "../config/config.js";
 import pool from "../config/database.js";
 import openai from "../config/openai.js";
 
 const chatService = {
+
+    saveChat: async (sessionId, chatText, byUser) => {
+        console.log('chatText: ', chatText);
+        try {
+            const saveChatQuery = `
+          INSERT INTO m_message (sid, message, by_user) VALUES (?,?,?);
+            `;
+            const results = await pool.query(saveChatQuery, [
+                sessionId,
+                chatText,
+                byUser
+            ]);
+            console.log(results[0]);
+            return results[0].affectedRows > 0;
+        } catch (error) {
+            console.error('Error in saveChat: ', error);
+            throw error;
+        }
+    },
+
     summarizeChat: async (text, callback) => {
-        text += "\nCan you summarize and include important points from first person perspective?";
+        text += "\nCan you summarize straight to the point and include important points from the second person's perspective using \"You\"?";
+        console.log('text: ', text);
         try {
             const chat = await openai.chat.completions.create({
                 model: "gpt-3.5-turbo",
-                messages: [{"role": "user", "content": text}]
+                messages: [{ "role": "user", "content": text }]
             });
             return callback(null, chat.choices[0].message.content);
         }
-        catch(err) {
+        catch (err) {
+            return callback(err, null);
+        }
+    },
+
+    saveSummary: async (messageId, summarizedText) => {
+        console.log('summarizedText: ', summarizedText);
+        try {
+            const saveSummaryQuery = `
+          INSERT INTO m_summary (mid, summary) VALUES (?,?);
+            `;
+            const results = await pool.query(saveSummaryQuery, [
+                messageId,
+                summarizedText
+            ]);
+            console.log(results[0]);
+            return results[0].affectedRows > 0;
+        } catch (error) {
+            console.error('Error in saveSummary: ', error);
+            throw error;
+        }
+    },
+
+    giveSuggestions: async (summarizedText, callback) => {
+        summarizedText += "\nCan you provide suggestions on how to improve the situation?";
+        console.log('text: ', summarizedText);
+        try {
+            const chat = await openai.chat.completions.create({
+                model: "gpt-3.5-turbo",
+                messages: [{ "role": "user", "content": summarizedText }]
+            });
+            return callback(null, chat.choices[0].message.content);
+        }
+        catch (err) {
             return callback(err, null);
         }
     }
